db87234b405a614c568f52ff1b23010e260be328
muhammadqasimjanjua162 <muhammadqasimjanjua162@gmail.com>
2024-12-28 16:49:13 +0500

    All Done

--- src/components/Games.tsx
+++ src/components/Games.tsx
@@ -9,21 +9,26 @@ interface Props {
 const Games = ({ gameQuery }: Props) => {
   const { data, error, isLoading } = useGames(gameQuery);
 
+  console.log(isLoading, "loadingssssss");
   return (
     <div>
       {error && <p>{error}</p>}
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3  xl:grid-cols-4 gap-4">
-        {isLoading
-          ? Array.from({ length: 9 }).map((_, index) => (
-              <div key={index}>
-                <GameCardSkeleton />
-              </div>
-            ))
-          : data.map((game) => (
-              <div key={game.id}>
-                <GameCard game={game} />
-              </div>
-            ))}
+      {isLoading &&
+          Array.from({ length: 9 }).map((_, index) => (
+            <div key={index}>
+              <GameCardSkeleton />
+            </div>
+          ))}
+          {!isLoading && data && data.length > 0 ? (
+          data.map((game) => (
+            <div key={game.id}>
+              <GameCard game={game} />
+            </div>
+          ))
+        ) : (
+          !isLoading && <p className="text-gray-500">No data found.</p>
+        )}
       </div>
     </div>
   );
